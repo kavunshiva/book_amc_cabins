254da6ceb5575d33adfbd9a0571affb4e91552b1
Isaac Anderson <kavunshiva@gmail.com>
2026-02-14 15:54:32 -0500

    prefer two spaces for indentation in JS

--- static/js/cabins.js
+++ static/js/cabins.js
@@ -1,87 +1,87 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const { cabinNames } = JSON.parse(document.querySelector('body').getAttribute('data'));
-    const startDateSelector = document.getElementById('start-date');
-    const daysSelector = document.getElementById('days');
-    const numberOfGuestsSelector = document.getElementById('guest-number');
-    const tableHeaderRow = document.getElementById('cabin-names');
-    const tableBody = document.getElementById('cabin-availabilities');
+  const { cabinNames } = JSON.parse(document.querySelector('body').getAttribute('data'));
+  const startDateSelector = document.getElementById('start-date');
+  const daysSelector = document.getElementById('days');
+  const numberOfGuestsSelector = document.getElementById('guest-number');
+  const tableHeaderRow = document.getElementById('cabin-names');
+  const tableBody = document.getElementById('cabin-availabilities');
 
-    const initializeOptions = () => {
-        for (let i = 1; i <= 40; i++) {
-            const daysOption = document.createElement('option');
-            daysOption.value = i;
-            daysOption.innerText = i;
-            daysSelector.appendChild(daysOption);
-        }
+  const initializeOptions = () => {
+    for (let i = 1; i <= 40; i++) {
+      const daysOption = document.createElement('option');
+      daysOption.value = i;
+      daysOption.innerText = i;
+      daysSelector.appendChild(daysOption);
+    }
 
-        for (let i = 1; i <= 10; i++) {
-            const numberOfGuestsOption = document.createElement('option');
-            numberOfGuestsOption.value = i;
-            numberOfGuestsOption.innerText = i;
-            numberOfGuestsSelector.appendChild(numberOfGuestsOption);
-        }
-    };
+    for (let i = 1; i <= 10; i++) {
+      const numberOfGuestsOption = document.createElement('option');
+      numberOfGuestsOption.value = i;
+      numberOfGuestsOption.innerText = i;
+      numberOfGuestsSelector.appendChild(numberOfGuestsOption);
+    }
+  };
 
-    const initializeDefaultValues = async () => {
-        const { minStartDate } = await fetch(`/api/min_start_date`).then((res) => res.json());
-        startDateSelector.setAttribute('min', minStartDate);
-        startDateSelector.setAttribute('value', minStartDate);
+  const initializeDefaultValues = async () => {
+    const { minStartDate } = await fetch(`/api/min_start_date`).then((res) => res.json());
+    startDateSelector.setAttribute('min', minStartDate);
+    startDateSelector.setAttribute('value', minStartDate);
 
-        daysSelector.value = 10;
-        numberOfGuestsSelector.value = 4;
-    };
+    daysSelector.value = 10;
+    numberOfGuestsSelector.value = 4;
+  };
 
-    const addOptionChangeListeners = () => {
-        [startDateSelector, daysSelector, numberOfGuestsSelector]
-            .forEach((element) => element.addEventListener('blur', () => renderAvailabilities()));
-    };
+  const addOptionChangeListeners = () => {
+    [startDateSelector, daysSelector, numberOfGuestsSelector]
+      .forEach((element) => element.addEventListener('blur', () => renderAvailabilities()));
+  };
 
-    const setTableHeaders = () => {
-        cabinNames.forEach((cabinName) => {
-            const cabinNameHeaderElement = document.createElement('td');
-            cabinNameHeaderElement.innerText = cabinName;
-            tableHeaderRow.appendChild(cabinNameHeaderElement);
-        });
-    };
+  const setTableHeaders = () => {
+    cabinNames.forEach((cabinName) => {
+      const cabinNameHeaderElement = document.createElement('td');
+      cabinNameHeaderElement.innerText = cabinName;
+      tableHeaderRow.appendChild(cabinNameHeaderElement);
+    });
+  };
 
-    const getParams = () => {
-        const params = new URLSearchParams();
-        cabinNames.forEach((cabinName) => params.append('cabinName', cabinName));
-        params.set('days', daysSelector.value);
-        params.set('numberOfGuests', numberOfGuestsSelector.value);
-        params.set('startDate', startDateSelector.value);
-        return params;
-    };
+  const getParams = () => {
+    const params = new URLSearchParams();
+    cabinNames.forEach((cabinName) => params.append('cabinName', cabinName));
+    params.set('days', daysSelector.value);
+    params.set('numberOfGuests', numberOfGuestsSelector.value);
+    params.set('startDate', startDateSelector.value);
+    return params;
+  };
 
-    const renderAvailability = (availability) => {
-        const availabilityRowElement = document.createElement('tr');
-        const dateElement = document.createElement('td');
-        dateElement.innerText = availability.date;
-        availabilityRowElement.appendChild(dateElement);
-        cabinNames.forEach((cabinName) => {
-            const cabinAvailabilityElement = document.createElement('td');
-            const cabinAvailable = availability[cabinName];
-            if (cabinAvailable) cabinAvailabilityElement.style.backgroundColor = 'green';
-            availabilityRowElement.appendChild(cabinAvailabilityElement);
-        });
-        tableBody.appendChild(availabilityRowElement);
-    };
+  const renderAvailability = (availability) => {
+    const availabilityRowElement = document.createElement('tr');
+    const dateElement = document.createElement('td');
+    dateElement.innerText = availability.date;
+    availabilityRowElement.appendChild(dateElement);
+    cabinNames.forEach((cabinName) => {
+      const cabinAvailabilityElement = document.createElement('td');
+      const cabinAvailable = availability[cabinName];
+      if (cabinAvailable) cabinAvailabilityElement.style.backgroundColor = 'green';
+      availabilityRowElement.appendChild(cabinAvailabilityElement);
+    });
+    tableBody.appendChild(availabilityRowElement);
+  };
 
-    const renderAvailabilities = async () => {
-        tableBody.innerHTML = '';
-        const loadingSpinner = document.getElementById('loading-spinner');
-        const availabilitiesTable = document.getElementById('availabilities-table');
-        loadingSpinner.style.display = 'flex';
-        availabilitiesTable.style.display = 'none';
-        const availabilities = await fetch(`/api/cabins?${getParams()}`).then((res) => res.json());
-        loadingSpinner.style.display = 'none';
-        availabilitiesTable.style.display = 'block';
-        availabilities.forEach((availability) => renderAvailability(availability));
-    };
+  const renderAvailabilities = async () => {
+    tableBody.innerHTML = '';
+    const loadingSpinner = document.getElementById('loading-spinner');
+    const availabilitiesTable = document.getElementById('availabilities-table');
+    loadingSpinner.style.display = 'flex';
+    availabilitiesTable.style.display = 'none';
+    const availabilities = await fetch(`/api/cabins?${getParams()}`).then((res) => res.json());
+    loadingSpinner.style.display = 'none';
+    availabilitiesTable.style.display = 'block';
+    availabilities.forEach((availability) => renderAvailability(availability));
+  };
 
-    initializeOptions();
-    await initializeDefaultValues();
-    addOptionChangeListeners();
-    setTableHeaders();
-    renderAvailabilities();
+  initializeOptions();
+  await initializeDefaultValues();
+  addOptionChangeListeners();
+  setTableHeaders();
+  renderAvailabilities();
 });
